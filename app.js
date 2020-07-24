22b3438438685c786481311a17935ea4dc271c80
Ranjit67 <sahooranjit519@gmail.com>
2020-07-24 23:11:51 +0530

    adding massage

--- app.js
+++ app.js
@@ -3,31 +3,42 @@ const nodemailer = require("nodemailer");
 const mongoose = require("mongoose");
 require('dotenv').config();
 const cors = require("cors");
-const path = require("path")
+const path=require("path");
 
 const app=express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI || process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connection.on("connected",()=>{
     console.log("The database is connected.");
 })
-
+mongoose.connection.on("err",()=>{
+    console.log(err);
+})
 app.use(require("./router/route.js"))
 
 
-if(process.env.NODE_ENV==='production'){
-app.use(express.static('frontend/build'))
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'frontend' , 'build', 'index.html')); //related path
+app.get("/succes",(req,res)=>{
+    res.send("Hello!! Ranjit");
 })
 
-}
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static("fronend/build"));
 
+
+    app.get('*',(req,res)=>{
+res.sendFile(path.join(__dirname,'frontend','public'));
+    })
+    
+}
 
 
 
 app.listen(process.env.PORT || 5000,()=>{
     console.log("The port 5000 is redy to start..");
 })
+
+
+
+// Database link:- mongodb+srv://admin-keshab:<password>@cluster0-y5crp.mongodb.net/LifesDB?retryWrites=true&w=majority
